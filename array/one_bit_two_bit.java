56dc533c1a07c45b2c554e760cdf8ec8b8868a3a
kishanraj04 <kishuraj1111@gmail.com>
2025-05-21 14:27:13 +0530

    pivot one_bit_two_bit

---
+++ array/one_bit_two_bit.java
@@ -0,0 +1,24 @@
+package array;
+
+public class one_bit_two_bit {
+    public static boolean oneTwoBit(int[] arr){
+
+        boolean flag = false;
+        for(int i=0;i<arr.length;i++){
+              if(i<arr.length-1){
+                if(arr[i]==1 && (arr[i+1]==0 || arr[i+1]==1)){
+                    flag=false;
+                    i++;
+                }
+              }
+              else{
+                flag=true;
+              }
+        }
+
+        return flag;
+    }
+    public static void main(String[] args) {
+        System.out.println(oneTwoBit(new int[]{1,1,0}));
+    }
+}
