e0a688af586561022733998851b8b3371f41ccc6
kishanraj04 <kishuraj1111@gmail.com>
2025-07-16 13:00:55 +0530

    1315. Sum of Nodes with Even-Valued Grandparent

--- tree/1315. Sum of Nodes with Even-Valued Grandparent.cpp
+++ tree/1315. Sum of Nodes with Even-Valued Grandparent.cpp
@@ -47,3 +47,37 @@ public:
         return sum;
     }
 };
+
+
+// sol 2
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+int sum = 0;
+void helper(TreeNode *root,TreeNode *p , TreeNode *gp){
+    if(!root) return;
+    
+    if(gp && gp->val%2==0){
+        sum+=root->val;
+    }
+
+    helper(root->left,root,p);
+    helper(root->right,root,p);
+    
+}
+
+public:
+    int sumEvenGrandparent(TreeNode* root) {
+        helper(root,nullptr,nullptr);
+        return sum;
+    }
+};
