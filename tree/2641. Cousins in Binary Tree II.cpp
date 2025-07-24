6f87df4256357b88e7d205d0f42b227b4dbf3908
kishanraj04 <kishuraj1111@gmail.com>
2025-07-24 11:10:56 +0530

    2641. Cousins in Binary Tree II

--- tree/2641. Cousins in Binary Tree II.cpp
+++ tree/2641. Cousins in Binary Tree II.cpp
@@ -55,3 +55,52 @@ public:
         return root;
     }
 };
+
+// sol 2
+class Solution {
+    void solv(TreeNode*& root,  vector<int>& l, int level) {
+        if (!root) return;
+
+        int ss = 0;
+        if (root->left) ss += root->left->val;
+        if (root->right) ss += root->right->val;
+
+        if (root->left) {
+            root->left->val = l[level] - ss;
+        }
+        if (root->right) {
+            root->right->val = l[level] - ss;
+        }
+
+        solv(root->left, l, level + 1);
+        solv(root->right, l, level + 1);
+    }
+
+public:
+    TreeNode* replaceValueInTree(TreeNode* root) {
+        vector<int> ls;
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            int s = q.size();
+            int sum = 0;
+
+            for (int i = 0; i < s; ++i) {
+                TreeNode* front = q.front();
+                q.pop();
+
+                sum += front->val;
+
+                if (front->left) q.push(front->left);
+                if (front->right) q.push(front->right);
+            }
+
+            ls.push_back(sum);
+        }
+
+        root->val = 0;
+        solv(root, ls, 1);
+        return root;
+    }
+};
