c4cf39028c827c77cf18d74fbec03b60cf48c525
kishanraj04 <kishuraj1111@gmail.com>
2025-08-08 20:23:30 +0530

    334. Increasing Triplet Subsequence

---
+++ greedy/334. Increasing Triplet Subsequence.c
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        int num1 = INT_MAX;
+        int num2 = INT_MIN;
+        int num3 = 0;
+
+        for(int v:nums){
+            num3 = v;
+            if(num3<=num1){
+                num1=num3;
+            }else if(num3<=num2){
+                num2=num3;
+            }else{
+                return true;
+            }
+        }
+        return false;
+    }
+};
